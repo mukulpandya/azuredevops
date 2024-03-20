32e3628d895524b6b7c6a9ffb59bc665cce31b53
LAPTOP-3R0U882B\Mukul Pandya <mukulpandya@live.com>
2024-03-20 15:49:24 +0530

    added main.tf

---
+++ weatherapi/Main.tf
@@ -0,0 +1,42 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "South India"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+    name = "weatherapi"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_address_type = "Public"
+    dns_name_label = "mukulpandyawa"
+    os_type = "Linux"
+
+    container {
+        name = "weatherapi"
+        image = "mukulpandya1981/weatherapi"
+          cpu = "1"
+          memory = "1"
+
+          ports {
+            port = 80
+            protocol = "TCP"
+          }
+    }
+}
